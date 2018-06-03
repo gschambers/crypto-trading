0b98c5567681f154bbba22a822a3fbef11221660
Gary Chambers <gary.chambers@privitar.com>
2018-06-03 20:02:04 +0100

    Move ticker logic to correct module

--- bridge.go
+++ bridge.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 // Bridge between clients and price stream ticks
 type Bridge struct {
 	clients    map[*Client]map[Instrument]bool
@@ -45,29 +39,5 @@ func (bridge *Bridge) run() {
 		case tick := <-bridge.ticker:
 			bridge.broadcast(tick)
 		}
-	}
-}
-
-func getNextTick() (tick Tick, err interface{}) {
-	tick = Tick{
-		Instrument: Instrument{From: "BTC", To: "USD"},
-		Price:      (rand.Int31n(5) + 100),
-	}
-
-	return tick, nil
-}
-
-func startTickStream(bridge *Bridge) {
-	for {
-		tick, err := getNextTick()
-
-		if err != nil {
-			fmt.Println("Error receiving tick:", err)
-			continue
-		}
-
-		bridge.ticker <- tick
-
-		time.Sleep(50 * time.Millisecond)
 	}
 }
--- ticker.go
+++ ticker.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 type Instrument struct {
 	From string `json:"from"`
 	To   string `json:"to"`
@@ -8,4 +14,28 @@ type Instrument struct {
 type Tick struct {
 	Instrument Instrument `json:"instrument"`
 	Price      int32      `json:"price"`
+}
+
+func getNextTick() (tick Tick, err interface{}) {
+	tick = Tick{
+		Instrument: Instrument{From: "BTC", To: "USD"},
+		Price:      (rand.Int31n(5) + 100),
+	}
+
+	return tick, nil
+}
+
+func startTickStream(bridge *Bridge) {
+	for {
+		tick, err := getNextTick()
+
+		if err != nil {
+			fmt.Println("Error receiving tick:", err)
+			continue
+		}
+
+		bridge.ticker <- tick
+
+		time.Sleep(50 * time.Millisecond)
+	}
 }
